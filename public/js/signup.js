dc7869a233c8d0883d607f24103c6b7ee45ed14b
Christopher Banta <chris.banta@rocketmail.com>
2023-09-01 01:01:12 -0500

    Added some finsihing touches, messed with seed data

--- public/js/signup.js
+++ public/js/signup.js
@@ -3,19 +3,24 @@ const signupFormHandler = async (event) => {
   const name = document.querySelector('#name-signup').value.trim();
   const email = document.querySelector('#email-signup').value.trim();
   const password = document.querySelector('#password-signup').value.trim();
-  if (name && email && password) {
-    const response = await fetch('/api/users', {
-      method: 'POST',
-      body: JSON.stringify({ name, email, password }),
-      headers: { 'Content-Type': 'application/json' },
-    });
-    if (response.ok) {
-      document.location.replace('/dashboard');
-    } else {
-      alert(response.statusText);
+  const passwordConfirm = document.querySelector('#password-confirm').value.trim();
+  if (password === passwordConfirm) {
+    if (name && email) {
+      const response = await fetch('/api/users', {
+        method: 'POST',
+        body: JSON.stringify({ name, email, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+      if (response.ok) {
+        document.location.replace('/dashboard');
+      } else {
+        alert(response.statusText);
+      }
     }
+  } else {
+    alert("Passwords do not match, try again!")
   }
-};
+}
   
 document
   .querySelector('.signup-form')
